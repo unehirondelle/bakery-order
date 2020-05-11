391d894c40c20340f8a70096d77cc8d623350368
Irina Plaksina <irene.plaksina@gmail.com>
2020-05-11 13:41:39 -0400

    added delete function for the cart

---
+++ public/delete.js
@@ -0,0 +1,9 @@
+$("#1").on("click", () => {
+    $("#honey-cake").attr("style", "display: none");
+});
+$("#2").on("click", () => {
+    $("#black-forest").attr("style", "display: none");
+});
+$("#3").on("click", () => {
+    $("#vanilla-berry").attr("style", "display: none");
+});
